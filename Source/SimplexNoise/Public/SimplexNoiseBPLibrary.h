2da181cc418e0fd130a276945b0c29151b9d77fe
Gigant <afanolovcic@gmail.com>
2019-07-21 01:57:35 +0200

    Added inFactor value to set noise frquency <best vales between 0.1 - 0.00001> Swiched to double from float internaly for more precision and performance

--- Source/SimplexNoise/Public/SimplexNoiseBPLibrary.h
+++ Source/SimplexNoise/Public/SimplexNoiseBPLibrary.h
@@ -32,10 +32,15 @@ class SIMPLEXNOISE_API USimplexNoiseBPLibrary : public UBlueprintFunctionLibrary
 private:
 
 	static unsigned char perm[];
-	static float  grad(int hash, float x);
-	static float  grad(int hash, float x, float y);
-	static float  grad(int hash, float x, float y, float z);
-	static float  grad(int hash, float x, float y, float z, float t);
+	static double  _grad(int hash, double x);
+	static double  _grad(int hash, double x, double y);
+	static double  _grad(int hash, double x, double y, double z);
+	static double  _grad(int hash, double x, double y, double z, double t);
+
+	static double _simplexNoise1D(double x);
+	static double _simplexNoise2D(double x, double y);
+	static double _simplexNoise3D(double x, double y, double z);
+	static double _simplexNoise4D(double x, double y, double z, double w);
 
 public:
 
@@ -43,44 +48,44 @@ public:
 		static void setNoiseSeed(const int32& newSeed);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoise1D(float x);
+		static float SimplexNoise1D(float x, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoise2D(float x, float y);
+		static float SimplexNoise2D(float x, float y, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoise3D(float x, float y, float z);
+		static float SimplexNoise3D(float x, float y, float z, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoise4D(float x, float y, float z, float w);
+		static float SimplexNoise4D(float x, float y, float z, float w, float inFactor = 1.f);
 
 	// Scaled by float value
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseScaled1D(float x , float s);
+		static float SimplexNoiseScaled1D(float x , float scaleOut = 1.f, float inFactor = 1.f);
 	
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseScaled2D(float x, float y, float s);
+		static float SimplexNoiseScaled2D(float x, float y, float scaleOut = 1.f, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseScaled3D(float x, float y, float z, float s);
+		static float SimplexNoiseScaled3D(float x, float y, float z, float scaleOut = 1.f, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseScaled4D(float x, float y, float z, float w, float s);
+		static float SimplexNoiseScaled4D(float x, float y, float z, float w, float scaleOut = 1.f, float inFactor = 1.f);
 
 	// Return value in Range between two float numbers
 	// Return Value is scaled by difference between rangeMin & rangeMax value
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseInRange1D(float x, float rangeMin, float rangeMax);
+		static float SimplexNoiseInRange1D(float x, float rangeMin, float rangeMax, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseInRange2D(float x, float y, float rangeMin, float rangeMax);
+		static float SimplexNoiseInRange2D(float x, float y, float rangeMin, float rangeMax, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseInRange3D(float x, float y, float z, float rangeMin, float rangeMax);
+		static float SimplexNoiseInRange3D(float x, float y, float z, float rangeMin, float rangeMax, float inFactor = 1.f);
 
 	UFUNCTION(BlueprintCallable, Category = "SimplexNoise")
-		static float SimplexNoiseInRange4D(float x, float y, float z, float w, float rangeMin, float rangeMax);
+		static float SimplexNoiseInRange4D(float x, float y, float z, float w, float rangeMin, float rangeMax, float inFactor = 1.f);
 
 };
 
